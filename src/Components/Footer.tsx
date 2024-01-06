029f97ceec06ae8247ed2d6dd096b81661f65078
Sameer <80711125+Sameony@users.noreply.github.com>
2024-01-06 13:34:18 +0530

    Fix twitter url

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className='bg-slate-900 w-full text-white pl-4 sm:pl-12 py-3 flex justify-between'>
         © Mohammed Sameer, 2023: No rights reserved XD
         <div className='flex'>
-            <a href="https://www.twitter.com/sameony" target='_blank' rel="noreferrer"><FaTwitter className='h-10 w-10 mx-5' /></a>
+            <a href="https://www.twitter.com/sameony1" target='_blank' rel="noreferrer"><FaTwitter className='h-10 w-10 mx-5' /></a>
             <a href="https://www.github.com/sameony" target='_blank' rel="noreferrer"><FaGithub className='h-10 w-10 mx-5' /></a>
             <a href="https://www.linkedin.com/in/sameony" target='_blank' rel="noreferrer"><FaLinkedin className='h-10 w-10 mx-5' /></a>
         </div>
